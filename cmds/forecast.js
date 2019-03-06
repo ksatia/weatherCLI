975afccf72383030a556cc7bc6eb740ca054070f
Karan Satia <ksatia@gmail.com>
2019-03-06 17:30:50 -0500

    updates code to include new parameter for networking call

--- cmds/forecast.js
+++ cmds/forecast.js
@@ -1,36 +1,20 @@
 const https = require('https')
+const weatherAPI = require("../utils/weather")
 
 module.exports = (args) => {
-    
-    const location = args.location || args.l
-    
-    const formattedLocation = location.split(' ').join('+')
-
-    const options = {
-        hostname: "api.openweathermap.org",
-        path: `/data/2.5/forecast?q=${formattedLocation}&appid=2064bed5488284ff55690da4bb180185&units=imperial`,
-        method: "GET"
+    try {
+        const location = args.location || args.l
+        const city = (location.split(','))[0]
+        const results = weatherAPI(location, "forecast", (err, data) => {
+            if (!err && data) {
+                console.log(data)
+                console.log(`Current weather in ${city}:`)
+                //console.log(`\t${temp}° and ${main}`)
+            } else {
+                console.error(err.message)
+            }
+        })
+    } catch (err){
+        console.error(err.message)
     }
-    
-    // request function calls back with response data
-    const req = https.request(options, (res) => {
-        var aggregator = ''
-        res.on('data', (d) => {
-            aggregator += d
-        })
-        res.on('end', () => {
-            const parsed = JSON.parse(aggregator)
-            
-            console.log(parsed)
-            // const {list} = parsed
-            // list.forEach ((forecastDay) => {
-            //     console.log(forecastDay.snow)
-            // })
-        })
-    })
-
-    req.on('error', (e) => {
-        console.error(e.message)
-    })
-    req.end()
 }
